97053e5c83af4046b5e43604e37c4afc4d8f0b5b
Niels Gregers Johansen <niels@timeboxer.dk>
2024-05-05 12:21:59 +0200

    baseline

---
+++ .koksmat/app/magicapp/register-service-endpoints.go
@@ -0,0 +1,19 @@
+/* 
+File have been automatically created. To prevent the file from getting overwritten
+set the Front Matter property ´keep´ to ´true´ syntax for the code snippet
+---
+keep: false
+---
+*/
+package magicapp
+
+import (
+	"github.com/magicbutton/magic-zones/services"
+	"github.com/nats-io/nats.go/micro"
+)
+
+func RegisterServiceEndpoints(root micro.Group) {
+    root.AddEndpoint("person", micro.HandlerFunc(services.HandlePersonRequests))
+        root.AddEndpoint("spacetype", micro.HandlerFunc(services.HandleSpaceTypeRequests))
+        root.AddEndpoint("space", micro.HandlerFunc(services.HandleSpaceRequests))
+    }
